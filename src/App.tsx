492835f13799d34d69d605c7994d603e0ef94e91
EricaReis <ericareis67@outlook.com>
2022-10-30 15:46:30 -0300

    Added inteface details

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,9 @@
-import { useState } from "react";
-import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
-import { NewTransactionModal } from "./components/NewTransactionModal";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { Transactions } from "./pages/Transactions";
 
 export function App() {
-  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
-    useState(false);
-
-  function handleCloseNewTransactionModal() {
-    setIsNewTransactionModalOpen(false);
-  }
-
   return (
     <>
       <ThemeProvider theme={defaultTheme}>
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,44 +3,43 @@ import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionsTable() {
-    useEffect(() => {
-        api('/transactions')
-            .then(response => console.log(response.data))
-    }, []);
+  useEffect(() => {
+    api("/transactions").then((response) => console.log(response.data));
+  }, []);
 
-    return(
-        <Container>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Titulo</th>
-                        <th>Valor</th>
-                        <th>Categoria</th>
-                        <th>Data</th>
-                    </tr>
-                </thead>
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Titulo</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
 
-                <tbody>
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td>R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td>R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                    <tr>
-                        <td>Desenvolvimento de Website</td>
-                        <td>R$12.000</td>
-                        <td>Desenvolvimento</td>
-                        <td>20/02/2021</td>
-                    </tr>
-                </tbody>
-            </table>
-        </Container>
-    );
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de site</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Website</td>
+            <td>R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
 }
