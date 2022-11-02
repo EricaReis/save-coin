479ea5a79cf24767205522327aa6f0fb711d586d
EricaReis <ericareis67@outlook.com>
2022-11-02 16:23:12 -0300

    Add HookForm

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -13,45 +13,42 @@ export const Overlay = styled(Dialog.Overlay)`
 export const Content = styled(Dialog.Content)`
   min-width: 32rem;
   border-radius: 6px;
-  padding: 2.3rem 3rem;
+  padding: 2.5rem 3rem;
   background: ${(props) => props.theme["gray-800"]};
-
   position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-
   form {
     margin-top: 2rem;
-
     display: flex;
     flex-direction: column;
     gap: 1rem;
-
     input {
       border-radius: 6px;
       border: 0;
       background: ${(props) => props.theme["gray-900"]};
       color: ${(props) => props.theme["gray-300"]};
       padding: 1rem;
-
       &::placeholder {
         color: ${(props) => props.theme["gray-500"]};
       }
     }
-
     button[type="submit"] {
-      height: 58px;
+      height: 50px;
       border: 0;
-      background: ${(props) => props.theme["gray-500"]};
+      background: ${(props) => props.theme["green-500"]};
       color: ${(props) => props.theme.white};
       font-weight: bold;
       padding: 0 1.25rem;
       border-radius: 6px;
-      margin-top: 1.5rem;
+      margin-top: 1.25rem;
       cursor: pointer;
-
-      &:hover {
+      &:disabled {
+        opacity: 0.6;
+        cursor: not-allowed;
+      }
+      &:not(:disabled):hover {
         background: ${(props) => props.theme["green-700"]};
         transition: background-color 0.2s;
       }
@@ -67,7 +64,7 @@ export const CloseButton = styled(Dialog.Close)`
   right: 1.5rem;
   line-height: 0;
   cursor: pointer;
-  color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-500"]};
 `;
 
 export const TransactionType = styled(RadioGroup.Root)`
@@ -94,26 +91,22 @@ export const TransactionTypeButton = styled(
   cursor: pointer;
   border: 0;
   color: ${(props) => props.theme["gray-300"]};
-
   svg {
     color: ${(props) =>
       props.variant === "income"
         ? props.theme["green-300"]
         : props.theme["red-300"]};
   }
-
   &[data-state="unchecked"]:hover {
     transition: background-color 0.2s;
     background: ${(props) => props.theme["gray-600"]};
   }
-
   &[data-state="checked"] {
     color: ${(props) => props.theme.white};
     background: ${(props) =>
       props.variant === "income"
         ? props.theme["green-500"]
         : props.theme["red-500"]};
-
     svg {
       color: ${(props) => props.theme.white};
     }
