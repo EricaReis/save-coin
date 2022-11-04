b3357306fa4c08d6129e474a92c7b693f95e91a8
EricaReis <ericareis67@outlook.com>
2022-11-03 21:17:42 -0300

    Removing duplicated API

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import { api } from "../../services/api";
+import { api } from "../../lib/axios";
+
 import { Container } from "./styles";
 
 export function TransactionsTable() {
