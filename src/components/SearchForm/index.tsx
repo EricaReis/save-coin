5bf75d1309c50a6eeecd3a5d24a34dc5898c5929
EricaReis <ericareis67@outlook.com>
2022-11-05 14:45:39 -0300

    Learning about memo

--- src/components/SearchForm/index.tsx
+++ src/components/SearchForm/index.tsx
@@ -5,10 +5,20 @@ import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 import { useContextSelector } from "use-context-selector";
+import { memo } from "react";
 
 const searchFormSchema = z.object({
   query: z.string(),
 });
+
+/*  
+* Memo:
+  - Olha se mudou algo nos hooks, nas props (deep comparison)
+  - Compara a versão anterior dos hooks e props
+  - Se mudou algo, ele vai permitir a nova renderização
+    A comparação do memo pode (e na maioria das vezes) vai
+  acabar sendo mais lenta do que a re-renderização do componente.
+*/
 
 type SearchFormInputs = z.infer<typeof searchFormSchema>;
 
@@ -46,3 +56,5 @@ export function SearchForm() {
     </SearchFormContainer>
   );
 }
+
+// export const SearchForm = memo(SearchFormComponent);
