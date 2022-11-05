615e0358070fd623f8436d391522833a8291b29a
EricaReis <ericareis67@outlook.com>
2022-11-05 14:17:59 -0300

    Adding use-context-selector

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -1,4 +1,5 @@
-import { createContext, ReactNode, useEffect, useState } from "react";
+import { ReactNode, useEffect, useState } from "react";
+import { createContext } from "use-context-selector";
 import { api } from "../lib/axios";
 
 interface Transaction {
@@ -16,36 +17,38 @@ interface CreateTransactionInput {
   category: string;
   type: "income" | "outcome";
 }
+
 interface TransactionContextType {
   transactions: Transaction[];
   fetchTransactions: (query?: string) => Promise<void>;
   createTransaction: (data: CreateTransactionInput) => Promise<void>;
 }
 
-export const TransactionsContext = createContext({} as TransactionContextType);
-
 interface TransactionsProviderProps {
   children: ReactNode;
 }
+
+export const TransactionsContext = createContext({} as TransactionContextType);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
   async function fetchTransactions(query?: string) {
-    const response = await api.get("/transactions", {
+    const response = await api.get("transactions", {
       params: {
         _sort: "createdAt",
         _order: "desc",
         q: query,
       },
     });
+
     setTransactions(response.data);
   }
 
   async function createTransaction(data: CreateTransactionInput) {
     const { description, price, category, type } = data;
 
-    const response = await api.post("/transactions", {
+    const response = await api.post("transactions", {
       description,
       price,
       category,
@@ -53,7 +56,7 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
       createdAt: new Date(),
     });
 
-    setTransactions((state) => [...state, response.data]);
+    setTransactions((state) => [response.data, ...state]);
   }
 
   useEffect(() => {
